0cd2bec598037ce4ef05f93a1925416b223550b3
John Widmaier <johnwid92@gmail.com>
2021-07-22 11:06:30 -0400

    created header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import WebFont from 'webfontloader'
 import Header from './components/header'
 
 import './App.css';
+
+WebFont.load({
+  google: {
+    families: ['Roboto Web:300,400,700', 'sans-serif']
+  }
+})
 
 function App() {
   return (
--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/header.css';
 
 const Header = () => {
+
+    const [isShrunk, setIsShrunk] = useState(false);
+
+    const buttonHandler = () => {
+        setIsShrunk(current => !current)
+    }
+
+    useEffect(() =>{}, [isShrunk])
+
     return (
-        <div>
-            <h1>Welcome to the poll app!</h1>
+        <div className={isShrunk ? "header shrunk" : "header"}>
+            <h1>They see me pollin!</h1>
+            <div className={isShrunk ? "new-poll-btn shrunk" : "new-poll-btn"} onClick={buttonHandler}>
+                {isShrunk? "Cancel" : "Create a new poll!"}
+            </div>
         </div>
     )
 }
