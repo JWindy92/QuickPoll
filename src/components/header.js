a65ce6401e17168fd9141809242727f080256427
John Widmaier <johnwid92@gmail.com>
2021-07-22 09:05:10 -0400

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Welcome to the poll app!</h1>
+        </div>
+    )
+}
+
+export default Header;
