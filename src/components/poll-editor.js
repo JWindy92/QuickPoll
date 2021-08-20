cfb6a59b9895a6cfd5f8a5d7cd5f0c306da593e5
John Widmaier <johnwid92@gmail.com>
2021-08-20 12:24:34 -0400

    reverting from redis

--- src/components/poll-editor.js
+++ src/components/poll-editor.js
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/poll-editor.css';
 
+const test_func = () => {
+
+    console.log('Test');
+}
+
 const PollEditor = (props) => {
 
     return(
         <div className={props.show ? "poll-editor show" : "poll-editor"}>
             <h1>Please enter a title</h1>
             <input className="title-input" type="text"></input>
+            <button onClick={test_func}>Create</button>
         </div>
     )
 
